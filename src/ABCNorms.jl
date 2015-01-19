836dc767bbc8f76a66a475c8833925ea9153236d
Dennis Prangle <dennis.prangle@gmail.com>
2015-01-19 12:08:45 +0000

    First commit

--- src/ABCNorms.jl
+++ src/ABCNorms.jl
@@ -1,5 +1,29 @@
 module ABCNorms
 
-# package code goes here
+using  StatsBase.WeightVec, StatsBase.cov, StatsBase.sample, StatsBase.mean
+import Base.show
+import Distributions.MvNormal, Distributions.rand, Distributions.pdf
 
-end # module
+export
+  ABCNorm,
+  Lp,
+  Euclidean,
+  Lognorm,
+  MahalanobisDiag,
+  MahalanobisEmp,
+  init,
+  evalnorm,
+  ABCInput,
+  RefTable,
+  ABCOutput,
+  sortABCOutput!,
+  rgk,
+  show,
+  doABCSMC
+  ##Also export MahalanobisGlasso when finished
+  
+include("norms.jl")
+include("abc_rejection.jl")
+include("abc_smc.jl")
+include("gk.jl")
+end
