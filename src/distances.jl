f48098bbc5acfbc3ba9860b5a7cc0e38d7282b4d
Dennis Prangle <dennis.prangle@gmail.com>
2015-03-18 14:58:43 +0000

    Remove RankDist

--- src/distances.jl
+++ src/distances.jl
@@ -17,7 +17,7 @@ function init(x::ABCDistance, sumstats::Array{Float64, 2})
     x
 end
 
-function init(x::ABCDistance, sumstats::Array{Float64, 2}, acceptable::Array{Bool, 1})
+function init(x::ABCDistance, sumstats::Array{Float64, 2}, acceptable::Array{Int32, 1})
     ##Default is to ignore "acceptable" argument
     init(x, sumstats)
 end
@@ -103,76 +103,4 @@ end
 function evaldist(x::MahalanobisEmp, s::Array{Float64, 1})
     absdiff = abs(x.sobs - s)
     (absdiff' * x.Ω * absdiff)[1]
-end
-
-type RankDist <: ABCDistance    
-    sobs::Array{Float64, 1} ##Observed summaries
-    N::Int32 ##How many particles to use
-    s_min::Array{Float64, 1} ##Smallest acceptable value of s
-    s_max::Array{Float64, 1} ##Largest acceptable value of s
-end
-
-function RankDist(sobs::Array{Float64, 1}, N::Int32)
-    nstats = length(sobs)
-    s_min = fill(-Inf, nstats)
-    s_max = fill(Inf, nstats)
-    RankDist(sobs, N, s_min, s_max)
-end
-
-function init(x::RankDist, sumstats::Array{Float64, 2})
-    ##Default is that all simulations are acceptable
-    (nstats, nsims) = size(sumstats)    
-    acceptable = fill(true, nsims)
-    init(x, sumstats, acceptable)
-end
-
-function init(x::RankDist, sumstats::Array{Float64, 2}, acceptable::Array{Bool, 1})
-    (nstats, nsims) = size(sumstats)
-    if (x.N >= sum(acceptable))
-        s_min = fill(-Inf, nstats)
-        s_max = fill(Inf, nstats)
-    else
-        ##marg_rank_dist is marginal signed rank distances of simulations to sobs
-        s = hcat(sumstats, x.sobs) 
-        marg_rank_dist = Array(Int32, (nstats, nsims))
-        for i in 1:nstats
-            rank = ordinalrank(vec(s[i,:]))
-            marg_rank_dist[i,:] = rank[1:nsims] - rank[nsims+1]
-        end
-        ##max_rank_dist is an absolute rank distance of simulations to sobs
-        ##It's set to Inf for simulations rejected under earlier distances
-        max_rank_dist = fill(Inf, nsims)
-        max_rank_dist[acceptable] = vec(maximum(abs(marg_rank_dist[:, acceptable]), 1))
-        toaccept = sortperm(max_rank_dist)[1:x.N]
-        sacc = sumstats[:,toaccept]
-        ##Find narrowest boundaries allowing x.N acceptances (plus any ties)
-        s_min = vec(minimum(sacc, 2))
-        s_max = vec(maximum(sacc, 2))
-        ##Expand to get widest boundaries allowing x.N acceptances (plus any ties)
-        ##TO DO: Not sure this code is very efficient!
-        for i in 1:nstats
-            z = [vec(sumstats[i, :]), -Inf, Inf]
-            s_min[i] = maximum(z[z.<s_min[i]])
-            s_max[i] = minimum(z[z.>s_max[i]])
-        end
-        ##DEBUGGING!
-        inbounds = 0
-        for i in 1:nsims
-            if (all(sumstats[:,i] .> s_min) & all(sumstats[:,i] .< s_max))
-                inbounds += 1
-            end
-        end
-        print("$inbounds sims are within the bounds\n")
-    end
-    RankDist(x.sobs, x.N, s_min, s_max)
-end
-    
-function evaldist(x::RankDist, s::Array{Float64, 1})
-    dist = 0.0 ##acceptance
-    if (any(s .<= x.s_min))
-        dist = 1.0 ##rejection
-    elseif (any(s .>= x.s_max))
-        dist = 1.0 ##rejection
-    end
-    return dist
 end
