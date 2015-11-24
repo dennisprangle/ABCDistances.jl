dc23e5f886f1da24fdf48d76f4e57d8ef1bae273
Dennis Prangle <dennis.prangle@gmail.com>
2015-11-24 11:32:28 +0000

    Update example to julia v0.4 and add pmc_dev run

--- examples/normal.jl
+++ examples/normal.jl
@@ -12,15 +12,18 @@ function plot_init(out::ABCPMCOutput, i::Int32)
     s1 = vec(ssim[1,1:n])
     s2 = vec(ssim[2,1:n])
     plot(s1, s2, ".", color=plot_cols[i])
+    return nothing
 end
 function plot_acc(out::ABCPMCOutput, i::Int32)
     w = out.abcdists[i].w
     h = out.thresholds[i]
+    h < Inf || return nothing
     ##Plot appropriate ellipse
-    θ = [0:0.1:6.3]
+    θ = 0:0.1:6.3
     x = (h/w[1])*sin(θ)+sobs[1]
     y = (h/w[2])*cos(θ)+sobs[2]
     plot(x, y, lw=3, color=plot_cols[i])
+    return nothing
 end
 
 ######################################################################
@@ -29,7 +32,7 @@ end
 ##Set up abcinput
 function sample_sumstats(pars::Array)
     success = true
-    stats = [pars[1] + 0.1*randn(1), randn(1)]
+    stats = [pars[1] + 0.1*randn(1); randn(1)]
     (success, stats)
 end
 
@@ -46,6 +49,8 @@ srand(20);
 pmcoutput_adapt = abcPMC(abcinput, 1000, 1/2, 50000, adaptive=true, store_init=true);
 srand(20);
 abcinput.abcdist = pmcoutput_adapt.abcdists[1];
+pmcoutput_adapt_dev = abcPMC_dev(abcinput, 1000, 1/2, 50000, h1=pmcoutput_adapt.thresholds[1], store_init=true);
+srand(20);
 pmcoutput_noadapt = abcPMC_comparison(abcinput, 1000, 1/2, 50000, h1=pmcoutput_adapt.thresholds[1], initialise_dist=false, store_init=true);
 srand(20);
 abcinput.abcdist = WeightedEuclidean(sobs);
@@ -56,8 +61,9 @@ pmcoutput_Mahalanobis = abcPMC(abcinput, 1000, 1/2, 50000, adaptive=true, store_
 
 ##Look at weights
 pmcoutput_noadapt.abcdists[1].w
-pmcoutput_noadapt2.abcdists[1].w ##Extremely similar
+pmcoutput_noadapt2.abcdists[1].w ##The same
 [pmcoutput_adapt.abcdists[i].w for i in 1:pmcoutput_adapt.niterations]
+[pmcoutput_adapt_dev.abcdists[i].w for i in 1:pmcoutput_adapt_dev.niterations]
 
 ##Plot simulations from each importance density and acceptance regions
 ##First for the two analyses shown in the paper
@@ -94,28 +100,42 @@ PyPlot.title("Algorithm 4 acceptance regions")
 PyPlot.tight_layout()
 PyPlot.savefig("normal_acc_regions.pdf")
 
-##Preceding plot for two analyses omitted from the paper
+##Preceding plot for three analyses omitted from the paper
 ##(Mahalanobis acc region omitted as this would need extra code)
-nits = min(pmcoutput_noadapt2.niterations, pmcoutput_Mahalanobis.niterations)
+nits = min(pmcoutput_noadapt2.niterations, pmcoutput_Mahalanobis.niterations, pmcoutput_adapt_dev.niterations)
 PyPlot.figure(figsize=(12,12))
-PyPlot.subplot(221)
+PyPlot.subplot(231)
 for i in 1:nits
     plot_init(pmcoutput_noadapt2, i)
 end
 PyPlot.xlabel(L"$s_1$")
 PyPlot.ylabel(L"$s_2$")
-PyPlot.subplot(222)
+PyPlot.subplot(232)
 for i in 1:nits
     plot_init(pmcoutput_Mahalanobis, i)
 end
 PyPlot.xlabel(L"$s_1$")
 PyPlot.ylabel(L"$s_2$") 
-PyPlot.subplot(223)
+PyPlot.subplot(233)
 for i in 1:nits
     plot_acc(pmcoutput_noadapt2, i)
 end
 PyPlot.xlabel(L"$s_1$")
 PyPlot.ylabel(L"$s_2$")
+PyPlot.subplot(234)
+for i in 1:nits
+    plot_init(pmcoutput_adapt_dev, i)
+end
+PyPlot.xlabel(L"$s_1$")
+PyPlot.ylabel(L"$s_2$")
+PyPlot.title("Algorithm 5 simulations")
+PyPlot.subplot(235)
+for i in 1:nits
+    plot_acc(pmcoutput_adapt_dev, i)
+end
+PyPlot.xlabel(L"$s_1$")
+PyPlot.ylabel(L"$s_2$")
+PyPlot.title("Algorithm 5 acceptance regions")
 PyPlot.tight_layout()
 
 ##Plot MSEs
@@ -136,8 +156,11 @@ PyPlot.savefig("normal_MSE.pdf");
 ##Add lines for other algorithms
 s3 = pmcoutput_noadapt2;
 s4 = pmcoutput_Mahalanobis;
+s5 = pmcoutput_adapt_dev;
 MSE3 = Float64[get_mses(s3.parameters[1,:,i], s3.weights[:,i])  for i in 1:nits];
 MSE4 = Float64[get_mses(s4.parameters[1,:,i], s4.weights[:,i])  for i in 1:nits];
+MSE5 = Float64[get_mses(s5.parameters[1,:,i], s5.weights[:,i])  for i in 1:nits];
 plot(pmcoutput_noadapt2.cusims[1:nits], log10(MSE3), "r-x");
 plot(pmcoutput_Mahalanobis.cusims[1:nits], log10(MSE4), "k-|");
-legend(["Algorithm 2","Algorithm 4","Alg 4 (non-adaptive)", "Alg 4 Mahalanobis"]);
+plot(pmcoutput_Mahalanobis.cusims[1:nits], log10(MSE5), "y-*");
+legend(["Algorithm 2","Algorithm 4","Alg 4 (non-adaptive)", "Alg 4 Mahalanobis", "Alg 5"]);
